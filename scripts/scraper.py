a4315da2cd9a55574a46a3876aae6a2aa5ce8586
Alex Mackenzie <44316926+AlexMackenzieWS@users.noreply.github.com>
2021-10-05 13:29:09 +0100

    added searching for specific query

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,4 +1,6 @@
 
+# Import libs
+
 import dotenv
 import sys
 import os
@@ -6,11 +8,64 @@ import tweepy
 
 dotenv.load_dotenv()
 
+# pulling env variables from local file
+
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+test = api.search(q=["vercel"], lang="en", count=20)
+
+for i in test:
+    print(i.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
