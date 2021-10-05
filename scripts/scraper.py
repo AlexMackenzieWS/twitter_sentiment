8960a25c5d8f2f533f30c209ff49338b30aceb01
Alex Mackenzie <44316926+AlexMackenzieWS@users.noreply.github.com>
2021-10-05 16:30:42 +0100

    added sentiment analysis for each tweet

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -5,6 +5,9 @@ import dotenv
 import sys
 import os
 import tweepy
+import nltk
+# nltk.download('punkt')
+from textblob import TextBlob
 
 dotenv.load_dotenv()
 
@@ -21,50 +24,22 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-test = api.search(q=["vercel"], lang="en", count=20)
+noOfTweet = 20
 
-for i in test:
-    print(i.text)
+tweets = api.search(q=["vercel"], lang="en", count=noOfTweet)
 
+test_list = []
 
+for tweet in tweets:
+    test_list.append(TextBlob(str(tweet.text)).sentiment)
 
+print(test_list)
 
+# words = "Hello there! how are ya"
 
+# test = TextBlob(words)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print(test.tags)
 
 
 
