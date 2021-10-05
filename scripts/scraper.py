56803e9c828f7fd63407b6a49fb050d38a93a023
Alex Mackenzie <44316926+AlexMackenzieWS@users.noreply.github.com>
2021-10-05 11:24:25 +0100

    authenticated into twitter

---
+++ scripts/scraper.py
@@ -0,0 +1,16 @@
+
+import dotenv
+import sys
+import os
+import tweepy
+
+dotenv.load_dotenv()
+
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
